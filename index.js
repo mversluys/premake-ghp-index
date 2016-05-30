bcb461e4a89053d595687c075aafc7d84c3f45d6
Mathew Versluys <mversluys@blizzard.com>
2016-05-29 20:54:45 -0700

    Changed callback to public site.

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ var GitHubStrategy = require('passport-github2').Strategy;
 passport.use(new GitHubStrategy({
 		clientID: process.env.GITHUB_CLIENT_ID,
 		clientSecret: process.env.GITHUB_CLIENT_SECRET,
-		callbackURL: "http://localhost:5000/api/auth-callback"
+		callbackURL: "http://premake-ghp.herokuapp.com/api/auth-callback"
+//		callbackURL: "http://localhost:5000/api/auth-callback"
 	},
 	function (accessToken, refreshToken, profile, done) {
 		// pause, dramatic
